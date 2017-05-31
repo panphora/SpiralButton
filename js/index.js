3a9fe1f29a6a4be305a25d7198eaca27a9c973e8
David Miranda <david@storylog.com>
2017-05-31 12:22:49 -0400

    put spiral behind text

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
-TweenMax.set(".spiral-bg", {transformOrigin: "50% 50%"});
-
-var tl = new TimelineMax({paused:true});
+var tl = new TimelineMax({visibility: "hidden", paused:true});
 
 tl
+.to(".spiral-bg", .2, {transformOrigin: "50% 50%", visibility: "visible"})
 .to(".spiral-bg", 1.8, {rotation: "-200", ease: Power0.easeNone})
-.to(".spiral-bg", 1.8, {scale: 200, ease: Power1.easeIn}, "-=2")
+.to(".spiral-bg", 1.8, {scale: 140, ease: Power1.easeIn}, "-=2")
+.to(".spiral-bg", 0, {visibility: "hidden"})
 .progress(1).progress(0);
 
 
 function goSpiral () {
-  TweenMax.set(".spiral-bg", {visibility: "visible"});
-
   tl
   .progress(0)
   .play();
