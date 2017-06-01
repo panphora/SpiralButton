d215ba14c680d895514bbd74c832358204e941ec
David Miranda <david@storylog.com>
2017-06-01 16:10:44 -0400

    with anime.js

--- js/index.js
+++ js/index.js
@@ -2,19 +2,36 @@
 
 // initialize elements
 var spiralButtonElem = document.querySelector(".spiral-button");
-var spiralBgElem = document.querySelector(".spiral-bg");
 var formElement = document.querySelector("form");
+// spiral grow calculation
+var spiralBgElem = document.querySelector(".spiral-bg");
 var spiralButtonFaceContainerElem = document.querySelector(".spiral-button-face-container");
-// spiral grow init
 var spiralButtonFaceOuterWidth = spiralButtonFaceContainerElem.offsetWidth;
 var tunnelExitWidth = Math.round(spiralButtonFaceOuterWidth) + 28;
 var spiralScaleValue = Math.round(tunnelExitWidth * (4/3));
 
-spiralBgElem.onCSSAnimationEnd(function () {
-  spiralButtonElem.classList.remove("pressed");
+var animation = anime({
+  autoplay: false,
+  targets: spiralBgElem,
+  scale: {
+    value: spiralScaleValue,
+    duration: 1500,
+    easing: "easeInQuad"
+  },
+  rotate: {
+    value: "-200deg",
+    duration: 1500,
+    easing: "linear"
+  },
+  begin: function () {
+    spiralButtonElem.classList.add("pressed");
+  },
+  complete: function (anim) {
+    spiralButtonElem.classList.remove("pressed");
+  }
 });
 
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
-  spiralButtonElem.classList.add("pressed");
+  animation.restart();
 });
