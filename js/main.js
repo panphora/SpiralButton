2b1fa7e4ba603afdd462579727be036b188d03a5
David Miranda <david@storylog.com>
2017-06-02 14:18:38 -0400

    added color schemes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 SpiralButton({
   buttonElement: document.querySelector("button"),
   buttonText: "Send",
+  colorScheme: "fruity",
   onComplete: function (buttonElement) {
-    console.log("button animation complete", buttonElement);
+    //console.log("button animation complete", buttonElement);
   }, 
   onStart: function (buttonElement) {
-    console.log("button animation started", buttonElement);
+    //console.log("button animation started", buttonElement);
   }
 });
