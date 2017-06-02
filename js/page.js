205836c236334aa97ae366746944d9d0a1af64e8
David Miranda <david@storylog.com>
2017-06-02 16:53:40 -0400

    working on front page, moved default theme styles into plugin

---
+++ js/page.js
@@ -0,0 +1,36 @@
+var buttonElements = document.querySelectorAll(".spiral-button");
+var colorSchemes = ["default","gold","pinkAndBlue","fruity","oceanWave","green","rainbow","brightRainbow","red","woodCabin","watermelon","cyan","wildColors","oceanGrass"];
+
+buttonElements.forEach(function (buttonElement, index) {
+  SpiralButton({
+    buttonElement: buttonElement,
+    buttonText: "Send",
+    colorScheme: colorSchemes[index]
+  });
+});
+
+
+
+
+function simulateClick (elem) {
+  elem.click();
+}
+
+doToEach(buttonElements, simulateClick, 400, true);
+
+function doToEach (arr, func, delay, repeat, index) {
+  var index = index || 0;
+  var lastItemIndex = arr.length - 1;
+
+  if (index <= lastItemIndex) {
+    setTimeout(function () {
+      func(arr[index]);
+      index += 1;
+      doToEach(arr, func, delay, repeat, index);
+    }, delay);
+  } else if (repeat) {
+    console.log(123);
+    index = 0;
+    doToEach(arr, func, delay, repeat, index);
+  }
+}
