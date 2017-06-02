18c040be15c97bdd173104b5e69bf58a494b2e06
David Miranda <david@storylog.com>
2017-06-01 21:31:49 -0400

    made it non-blurry in safari!!!

--- js/index.js
+++ js/index.js
@@ -3,18 +3,29 @@
 // initialize elements
 var spiralButtonElem = document.querySelector(".spiral-button");
 var formElement = document.querySelector("form");
-// spiral grow calculation
+
+
+// SPIRAL GROW CALCULATION
 var spiralBgElem = document.querySelector(".spiral-bg");
 var spiralButtonFaceContainerElem = document.querySelector(".spiral-button-face-container");
 var spiralButtonFaceOuterWidth = spiralButtonFaceContainerElem.offsetWidth;
+var spiralButtonFaceOuterHeight = spiralButtonFaceContainerElem.offsetHeight;
 // desiredTunnelExitWidth will probably range from 100px to about 400px
 var desiredTunnelExitWidth = spiralButtonFaceOuterWidth + 28;
-// initialSpiralBgWidth will probably range from 4.4px to 17.8px
-var initialSpiralBgWidth = (desiredTunnelExitWidth * (20/3)) / 150;
+// initialSpiralBgSize will probably range from 4.4px to 17.8px
+var initialSpiralBgSize = (desiredTunnelExitWidth * (20/3));
 
-// set initial width of spiral so it adapts to text size
-spiralBgElem.style.width = initialSpiralBgWidth + "px";
-spiralBgElem.style.height = initialSpiralBgWidth + "px";
+// set initial width of spiral to adapt to text size
+var cssTextArr = [];
+cssTextArr.push("width: " + initialSpiralBgSize + "px;");
+cssTextArr.push("height: " + initialSpiralBgSize + "px;");
+// center spiral over button face
+cssTextArr.push("left: " + (spiralButtonFaceOuterWidth/2 - initialSpiralBgSize/2) + "px;");
+cssTextArr.push("top: " + (spiralButtonFaceOuterHeight/2 - initialSpiralBgSize/2) + "px;");
+spiralBgElem.style.cssText += cssTextArr.join(" ");
+
+
+// EVENTS
 
 spiralBgElem.onCSSAnimationEnd(function () {
   spiralButtonElem.classList.remove("pressed");
